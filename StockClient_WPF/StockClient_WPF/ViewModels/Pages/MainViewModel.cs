c32d316b6992400d4caffdf1dda4fd54e0e1c661
smallj <smallj1044@gmail.com>
2026-05-13 01:13:40 +1000

    MainViewModel에서 필요한 데이터 수신 및 View와 연결 처리

--- StockClient_WPF/StockClient_WPF/ViewModels/Pages/MainViewModel.cs
+++ StockClient_WPF/StockClient_WPF/ViewModels/Pages/MainViewModel.cs
@@ -1,10 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
+using StockClient_WPF.Interfaces;
+using StockClient_WPF.Views.Pages;
+using StockServiceProtocol;
 
 namespace StockClient_WPF.ViewModels.Pages
 {
-    public class MainViewModel
+    public class MainViewModel : ObservableObject
     {
+        private IServerConnection<Packet, Packet> _serverConnection;
+
+        [ObservableProperty]
+        private ResItemDto _items;
+
+        //[ObservableProperty]
+        //private ResStockDto? _stocks;
+
+
+        public MainViewModel(IServerConnection<Packet, Packet> serverConnection)
+        {
+            this._serverConnection = serverConnection;
+
+            UpdateItems();
+
+        }
+
+        [RelayCommand]
+        public void AddItem()
+        {
+            var itemFormWindow = App.Services.GetRequiredService<ItemForm>();
+            itemFormWindow.Show();
+
+        }
+
+        [RelayCommand]
+        public void AddStock()
+        {
+            var stockFormWindow = App.Services.GetRequiredService<StockForm>();
+            stockFormWindow.Show();
+        }
+
+        private void UpdateItems()
+        {
+            Packet packet = new Packet
+            {
+                Type = PacketType.PrintItem,
+                GetItemsReq = new GetItemsRequest()
+            };
+
+            this._serverConnection.Send(packet);
+            Packet res = this._serverConnection.Receive(1024);
+
+
+            this._items = res.GetItemsRes.ResItemDto;
+        }
+
+        private void UpdateStocks()
+        {
+
+        }
     }
-}
