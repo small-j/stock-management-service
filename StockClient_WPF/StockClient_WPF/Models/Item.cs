2af9afa53ab4b154667f6aa2d53c32ddc00a6f17
smallj <smallj1044@gmail.com>
2026-05-30 21:13:35 +0900

    누락된 item model 파일

---
+++ StockClient_WPF/StockClient_WPF/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockClient_WPF.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ItemType { get; set; }
+    }
+}
